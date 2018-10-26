84bdbf4915537108eb9c101fef54687ecc7d1bad
hopefulChina <784667332@qq.com>
2018-10-26 09:39:44 +0800

    第三次提交

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,8 +1,16 @@
+import testDrive from '../../modules/test-drive'
 const app = getApp()
+
 Page({
   data: {
     slides: null,
-    entities:null
+    entities: null
+  },
+  testDrive,
+  readMore(event) {
+    wx.navigateTo({
+      url: `/pages/vehicles/show?id=${event.target.dataset.id}`
+    })
   },
   onLoad() {
     this.setData({
--- pages/vehicles/show.js
+++ pages/vehicles/show.js
@@ -1,16 +1,37 @@
-const app =getApp()
+import testDrive from '../../modules/test-drive'
+const app = getApp()
+
 Page({
-  data:{
-    entity:null
+  data: {
+    entity: null
   },
-  onLoad(){
-    const id =3
-    const entity = app.globalData.vehicles.filter((item) => {
-      return item.id ==id
+  testDrive,
+  preview(event) {
+    const slidesName = event.target.dataset.slides
+    const index = event.target.dataset.index
+
+    const slides = this.data.entity.meta[slidesName]
+    const images = []
+
+    slides.map((item) => {
+      images.push(item.image)
     })
+
+    wx.previewImage({
+      urls: images,
+      current: images[index]
+    })
+  },
+  onLoad(options) {
+    const id = options.id
+    const entity = app.globalData.vehicles.filter((item) => {
+      return item.id == id
+    })
+
     this.setData({
       entity: entity[0]
     })
+
     wx.setNavigationBarTitle({
       title: this.data.entity.header
     })
